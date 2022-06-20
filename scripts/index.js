74a7683f6e0de206e900a9b4f96e62dd919d67b4
Artem U <artemm3dlan2@gmail.com>
2022-06-20 21:39:37 +0300

    end

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,21 @@
-let profileButton = document.querySelector(".profile__button");
-const popup = document.querySelector(".popup");
-const popupClose = document.querySelector(".popup__close");
-let formElement = document.querySelector(".popup__content");
-let nameInput = document.querySelector(".popup__name");
-let jobInput = document.querySelector(".popup__status");
-let profileName = document.querySelector(".profile__name");
-let profileStatus = document.querySelector(".profile__status");
+let profileButton = document.querySelector('.profile__button');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__content');
+let nameInput = document.querySelector('.popup__input_field_name');
+let jobInput = document.querySelector('.popup__input_field_status');
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
 
 function openPopup() {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileStatus.textContent;
 }
 
-
-
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
-
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -26,6 +23,7 @@ function formSubmitHandler(event) {
   profileStatus.textContent = jobInput.value;
   closePopup();
 }
-profileButton.addEventListener("click", openPopup);
-popupClose.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+
+profileButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
