59adddec40337b5f5affc5e8ca2816c7f0c36923
Artem U <artemm3dlan2@gmail.com>
2022-06-26 01:31:59 +0300

    initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,47 @@ let jobInput = document.querySelector('.popup__input_field_status');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
+const elements = document.querySelector('.elements');
+const directorTemplate = document.querySelector('.element-template').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+initialCards.forEach(function(element) {
+  const card = directorTemplate.cloneNode(true);
+
+  card.querySelector('.element__title').textContent = element.name;
+  card.querySelector('.element__image').src = element.link;
+
+  elements.append(card);
+})
+
+
+
+
 function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
